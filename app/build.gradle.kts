e6db833daccaca6405d601e934aa782428b78918
BiaHoi-BaChien <pachosugi@gmail.com>
2026-05-26 11:59:22 +0700

    chore(version): 0.0.3へ更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 import java.util.Properties
 
-val appVersionName = "0.0.1"
+val appVersionName = "0.0.3"
 val releaseKeystorePropertiesFile = rootProject.file("keystore.properties")
 val releaseKeystoreProperties = Properties().apply {
     if (releaseKeystorePropertiesFile.isFile) {
