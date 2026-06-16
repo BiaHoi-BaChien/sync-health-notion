cf6fd74d0701318bfc1e1bcefb122c63babccea2
BiaHoi-BaChien <sugi@clb-biahoi.net>
2026-06-16 14:48:05 +0700

    chore: bump release version to 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 import java.util.Properties
 
-val appVersionName = "0.1.7"
+val appVersionName = "0.1.8"
 val appVersionCode = versionCodeFrom(appVersionName)
 val releaseKeystorePropertiesFile = rootProject.file("keystore.properties")
 val releaseKeystoreProperties = Properties().apply {
